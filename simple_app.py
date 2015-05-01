24b0af66fe22f97d7c43fcb31be1097ee9d8f6b0
Darryl Tharpe <darryl.tharpe@turner.com>
2015-05-01 13:45:32 -0400

    commit

---
+++ simple_app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<name>')
+def index(name="flask_test"):
+	return render_template("index.html", name=name)
+
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+	context = {'num1': num1, 'num2': num2}
+	return render_template("add.html", **context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.run(debug=True, port=8000, host="127.0.0.1")
